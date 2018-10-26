67c14ba762d64a75bb8de801bea8e52c3511f9c9
Kira <kira.anleu@hotmail.com>
2018-10-26 14:27:04 +0000

    Adding categories and adding it to a new category

--- taskmanager.py
+++ taskmanager.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template,request, redirect
+from flask import Flask, render_template,request, redirect,url_for
 import os
 from flask_pymongo import PyMongo
-
+from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("DB_NAME")
@@ -9,30 +9,56 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
+def get_category_names():
+   categories = []
+   for category in mongo.db.collection_names():
+           if not category.startswith("system."):
+               categories.append(category)
+   return categories
+
 @app.route("/")
 def get_tasks():
-    tasks = mongo.db.tasks.find()
-    return render_template("tasks.html", tasks=tasks)
-    
-@app.route("/tasks/<collection_name>")
-def get_tasks_by_collection(collection_name):
-    tasks = mongo.db[collection_name].find()
-    return render_template("tasks.html", tasks=tasks)
-    
+   tasks = mongo.db.tasks.find()
+   return render_template("tasks.html", tasks=tasks)
+
+@app.route("/tasks/<category_name>")
+def get_tasks_by_category(category_name):
+   tasks = mongo.db[category_name].find()
+   return render_template("tasks.html", tasks=tasks)
+
 @app.route("/add_task", methods=["GET", "POST"])
 def add_task():
+
+   if request.method=="POST":
+       form_values = request.form.to_dict()
+       form_values["is_urgent"] = "is_urgent" in form_values
+       category = form_values["category_name"]
+       mongo.db[category].insert_one(form_values)
+       return redirect("/")
+   else:
+       categories = get_category_names()
+
+       return render_template("addtask.html", categories=categories)
+       
+@app.route('/tasks/<category>/<task_id>/edit', methods=["GET", "POST"])
+def edit_task(category, task_id):
     if request.method=="POST":
         form_values = request.form.to_dict()
         form_values["is_urgent"] = "is_urgent" in form_values
-        category = form_values["category_name"]
-        mongo.db[category].insert_one(form_values)
-        return redirect("/")
-    else:
-        categories = []
-        for category in mongo.db.collection_names():
-            if not category.startswith("system."):
-                categories.append(category)
+        mongo.db[category].update({"_id": ObjectId(task_id)}, form_values)
         
-        return render_template("addtask.html", categories=categories)
+        if form_values["category_name"] != category:
+            the_task = mongo.db[category].find_one({"_id": ObjectId(task_id)})
+            mongo.db[category].remove(the_task)
+            mongo.db[form_values["category_name"]].insert(the_task)
+            
+        return redirect(url_for("get_tasks_by_category", category_name=form_values["category_name"]))
+    else:
+        the_task =  mongo.db[category].find_one({"_id": ObjectId(task_id)})
+        categories = get_category_names()
+        return render_template('edittask.html', task=the_task, categories=categories)
+       
+
+
 if __name__ == "__main__":
-        app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
+       app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
